69012e6d8d94b3b5f2ff39c722ff11dfa69d2eea
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-01-25 13:12:32 +0200

    Add a moving border below the navigation bar

--- jechel-dev-react/src/components/main/NavigationBar.js
+++ jechel-dev-react/src/components/main/NavigationBar.js
@@ -1,24 +1,35 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import mainStyles from './mainStyles';
 import { Stack } from '@mui/material';
 
 const NavigationBar = () => {
+
+  const location = useLocation();
+
+  const navLinkStyle = {
+    ...mainStyles.linkStyle,
+    position: 'relative',
+    textDecoration: 'none',
+    color: 'inherit', // Ensure the link text color is inherited from the parent
+  };
+
+  const activeLinkStyle = {
+    borderBottom: '2px solid #000', // Solid line for the selected link
+  };
+
   return (
     <Container>
       <Box sx={mainStyles.horizontalCenterStyle}>
         <nav>
-          <Stack direction="row" spacing={3}>
-            <Link to='/' style={mainStyles.linkStyle}>Latest</Link>
-            <Link to='/moto' style={mainStyles.linkStyle}>Moto</Link>
-            <Link to='/aboutMe' style={mainStyles.linkStyle}>About Me</Link>
-          </Stack>
 
-          {/* <Link to='/mobileApps' style={{ marginRight: '10px' }}>Mobile Apps</Link>
-          <Link to='/videoGames' style={{ marginRight: '10px' }}>Video Games</Link>
-          <Link to='/movies' style={{ marginRight: '10px' }}>Movies</Link>
-          <Link to='/contact' style={{ marginRight: '10px' }}>Contact</Link> */}
+          <Stack direction="row" spacing={3}>
+            <Link to='/' style={{ ...navLinkStyle, ...(location.pathname === '/' && activeLinkStyle) }}>Latest</Link>
+            <Link to='/moto' style={{ ...navLinkStyle, ...(location.pathname === '/moto' && activeLinkStyle) }}>Moto</Link>
+            <Link to='/aboutMe' style={{ ...navLinkStyle, ...(location.pathname === '/aboutMe' && activeLinkStyle) }}>About Me</Link>
+          </Stack>
+          {/* Mobile Apps Video Games Movies Contact */}
         </nav>
       </Box>
     </Container>
