3e353ca2a3d9638b827b8071c1718583cc8a1a59
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-01-25 18:38:12 +0200

    Add shadow border to the YT component

--- jechel-dev-react/src/components/common/youTube/youTubeStyles.js
+++ jechel-dev-react/src/components/common/youTube/youTubeStyles.js
@@ -8,6 +8,7 @@ const latestStyles = {
     maxWidth: '1000px', // Maximum width is 1000px
     margin: '0 auto', // Center the Paper horizontally
     padding: '20px', // Add padding as needed
+    boxShadow: '0 0 100px 0 rgba(60, 7, 83, 0.5)' // shadow border
   },
 
   imgContainerStyle: {
