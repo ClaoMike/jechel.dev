66f8591094a523f23e9c1bee56935f887b5192bb
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-01-26 17:18:30 +0200

    Update ModeSwitch with colours ( for now)

--- jechel-dev-react/src/components/main/ModeSwitch.js
+++ jechel-dev-react/src/components/main/ModeSwitch.js
@@ -1,36 +1,54 @@
 import React, { useState } from 'react';
-import { Switch } from '@mui/joy';
+import { Switch, switchClasses } from '@mui/joy';
 import WbSunnyRoundedIcon from '@mui/icons-material/WbSunnyRounded';
 import NightsStayRoundedIcon from '@mui/icons-material/NightsStayRounded';
 
 const ModeSwitch = () => {
   const [checked, setChecked] = useState(false);
 
-  const lightStyle = {
-    color: '#000000' 
+  const lightModeIconStyle = {
+    color: '#D04848' 
   };
 
+  const darktModeIconStyle = {
+    color: '#FDE767' 
+  };
+  
   return (
     <>
-            <Switch 
-                // color={checked ? 'primary' : 'danger'}
-                slotProps={{
-                    input: { 'aria-label': 'Dark mode' },
-                    thumb: {
-                    children: checked ? <NightsStayRoundedIcon style={lightStyle}/> : <WbSunnyRoundedIcon style={lightStyle}/>,
+        <Switch 
+            slotProps={{
+                input: { 'aria-label': 'Dark mode' },
+                thumb: {
+                    children: checked ? <NightsStayRoundedIcon style={darktModeIconStyle}/> : <WbSunnyRoundedIcon style={lightModeIconStyle}/>,
+                },
+            }}
+            checked={checked}
+            onChange={(event) => setChecked(event.target.checked)}
+            sx={{
+                // sizes
+                "--Switch-trackRadius": "20px",
+                "--Switch-trackWidth": "59px",
+                "--Switch-trackHeight": "30px",
+                "--Switch-thumbSize": "24px",
+
+                // colours
+                // unchecked (default)
+                '--Switch-trackBackground': '#83C0C1', 
+                '--Switch-thumbBackground': '#FFFFFF',
+                '&:hover': {
+                    '--Switch-trackBackground': '#0A1D56',
+                },
+                
+                // checked
+                [`&.${switchClasses.checked}`]: {
+                    '--Switch-trackBackground': '#0A1D56',
+                    '&:hover': {
+                        '--Switch-trackBackground': '#83C0C1',
                     },
-                }}
-                checked={checked}
-                onChange={(event) => setChecked(event.target.checked)}
-                sx={{           
-                    "--Switch-trackRadius": "20px",
-                    "--Switch-trackWidth": "59px",
-                    "--Switch-trackHeight": "30px",
-                    "--Switch-thumbSize": "24px",
-            // //         '--Switch-thumbBackground': 'rgb(96, 94, 92)',
-            // // '--Switch-trackBorderColor': 'rgb(96, 94, 92)',
-                }}
-            />
+                },
+            }}
+        />
     </>
   );
 };
