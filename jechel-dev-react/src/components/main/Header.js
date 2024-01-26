86ed8394fd8f161694509b417fa2feb1b80e49e1
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-01-26 15:37:29 +0200

    Install Joy UI and MAterial Icons

--- jechel-dev-react/src/components/main/Header.js
+++ jechel-dev-react/src/components/main/Header.js
@@ -1,11 +1,27 @@
 import Title from './Title'
 import Subtitle from './Subtitle'
+import Switch from '@mui/joy/Switch';
+import DarkMode from '@mui/icons-material/DarkMode';
 
 const Header = () => {
   return (
     <div>
         <Title />
         <Subtitle />
+
+        <Switch
+          size="lg"
+          slotProps={{
+            input: { 'aria-label': 'Dark mode' },
+            thumb: {
+              children: <DarkMode />,
+            },
+          }}
+          sx={{
+            '--Switch-thumbSize': '16px',
+          }}
+          />
+
     </div>
   )
 }
