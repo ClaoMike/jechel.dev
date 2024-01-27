4ac556db6a695f4fab757c2184a199a782f31273
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-01-27 15:40:36 +0200

    Simplify ExternalLink class

--- jechel-dev-react/src/components/common/ExternalLink.js
+++ jechel-dev-react/src/components/common/ExternalLink.js
@@ -1,28 +1,12 @@
 import PropTypes from 'prop-types'
-import { useState } from 'react';
-
-import appStyle from '../../AppStyle';
 
 const ExternalLink = ({link, text, icon}) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
   return (
     <>
         <a 
           href={link} 
           target="_blank" 
           rel="noopener noreferrer" 
-          style={isHovered? appStyle.linkHoverColour : appStyle.linkStyle}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
           >{text} {icon}
         </a>
     </>
