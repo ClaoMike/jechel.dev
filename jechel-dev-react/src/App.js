f60d9b046f71b32c8df71edb759e9430bb05aeca
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-05 19:54:49 +0200

    Refactoring to a simple dark'light mode switch

--- jechel-dev-react/src/App.js
+++ jechel-dev-react/src/App.js
@@ -6,12 +6,30 @@ import Latest from "./components/latest/Latest";
 import Moto from "./components/moto/Moto";
 import AboutMe from "./components/aboutMe/AboutMe";
 
+import useLocalStorage from 'use-local-storage';
+import Switch from "react-switch";
+import { useState } from 'react';
+
 function App() {
+
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light' )
+
+  const [checked, setChecked] = useState(false);
+
+  const switchTheme = ()  => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setChecked(!checked);
+    setTheme(newTheme);
+  }
   return (
     <Router>
-      <div>
+      <div className="App" data-theme={theme}>
         <Header />
         <NavigationBar />
+
+        <Switch onChange={switchTheme} checked={checked} />
+        
 
         <Routes>
           <Route path='/' element={<Latest />} />
