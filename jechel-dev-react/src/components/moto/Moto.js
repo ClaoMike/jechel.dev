b9a0b88d3e8a7044e466ed5fe5016e99d5e8d62e
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-06 23:27:18 +0200

    Fix all YT episodes

--- jechel-dev-react/src/components/moto/Moto.js
+++ jechel-dev-react/src/components/moto/Moto.js
@@ -14,52 +14,52 @@ const Moto = () => {
   const episode_7_youTubeLink = 'https://www.youtube.com/watch?v=i6CfPYxzAFM&t=902s';
 
   const episode_6_imageUrl = 'https://img.youtube.com/vi/B5IOy-K5SxU/0.jpg';
-  const episode_6_altDescription = 'Description of the image';
+  const episode_6_altDescription = 'Blasting through Louches';
   const episode_6_title = 'Episode 6';
-  const episode_6_location = 'Germany';
-  const episode_6_description = 'Blasting on the highway on my way from Manchester, UK to Bucharest, Romania';
+  const episode_6_location = 'Louches, France';
+  const episode_6_description = 'Blasting through Louches';
   const episode_6_youTubeLink = 'https://www.youtube.com/watch?v=B5IOy-K5SxU&t=762s';
 
   const episode_5_imageUrl = 'https://img.youtube.com/vi/tOOPo1DSvDw/0.jpg';
-  const episode_5_altDescription = 'Description of the image';
+  const episode_5_altDescription = 'Leaving Manchester, on my way to Bucharest';
   const episode_5_title = 'Episode 5';
-  const episode_5_location = 'Germany';
-  const episode_5_description = 'Blasting on the highway on my way from Manchester, UK to Bucharest, Romania';
+  const episode_5_location = 'Manchester, UK';
+  const episode_5_description = 'Leaving Manchester, on my way to Bucharest';
   const episode_5_youTubeLink = 'https://www.youtube.com/watch?v=tOOPo1DSvDw&t=914s';
 
   const episode_4_imageUrl = 'https://img.youtube.com/vi/b6zmadoD7JA/0.jpg';
-  const episode_4_altDescription = 'Description of the image';
+  const episode_4_altDescription = 'Blasting through Glossop';
   const episode_4_title = 'Episode 4';
-  const episode_4_location = 'Germany';
-  const episode_4_description = 'Blasting on the highway on my way from Manchester, UK to Bucharest, Romania';
+  const episode_4_location = 'Glossop, UK';
+  const episode_4_description = 'Blasting through Glossop';
   const episode_4_youTubeLink = 'https://www.youtube.com/watch?v=b6zmadoD7JA&t=11s';
 
   const episode_3_imageUrl = 'https://img.youtube.com/vi/ajYExKvoLKE/0.jpg';
-  const episode_3_altDescription = 'Description of the image';
+  const episode_3_altDescription = 'Blasting through Wigan';
   const episode_3_title = 'Episode 3';
-  const episode_3_location = 'Germany';
-  const episode_3_description = 'Blasting on the highway on my way from Manchester, UK to Bucharest, Romania';
+  const episode_3_location = 'Wigan, UK';
+  const episode_3_description = 'Blasting through Wigan';
   const episode_3_youTubeLink = 'https://www.youtube.com/watch?v=ajYExKvoLKE&t=10s';
 
   const episode_2_imageUrl = 'https://img.youtube.com/vi/zdXuifpi4Nw/0.jpg';
-  const episode_2_altDescription = 'Description of the image';
+  const episode_2_altDescription = 'Blasting through Wigan';
   const episode_2_title = 'Episode 2';
-  const episode_2_location = 'Germany';
-  const episode_2_description = 'Blasting on the highway on my way from Manchester, UK to Bucharest, Romania';
+  const episode_2_location = 'Wigan, UK';
+  const episode_2_description = 'Blasting through Wigan';
   const episode_2_youTubeLink = 'https://www.youtube.com/watch?v=zdXuifpi4Nw&t=23s';
 
   const episode_1_imageUrl = 'https://img.youtube.com/vi/A5Eph62uQ38/0.jpg';
-  const episode_1_altDescription = 'Description of the image';
+  const episode_1_altDescription = 'Blasting through Manchester';
   const episode_1_title = 'Episode 1';
-  const episode_1_location = 'Germany';
-  const episode_1_description = 'Blasting on the highway on my way from Manchester, UK to Bucharest, Romania';
+  const episode_1_location = 'Manchester, UK';
+  const episode_1_description = 'Blasting through Manchester';
   const episode_1_youTubeLink = 'https://www.youtube.com/watch?v=A5Eph62uQ38&t=5s';
 
   const episode_trailer_imageUrl = 'https://img.youtube.com/vi/-dgtesVWdpc/0.jpg';
-  const episode_trailer_altDescription = 'Description of the image';
+  const episode_trailer_altDescription = 'A short preview of the best moto riding series on YouTube.';
   const episode_trailer_title = 'Trailer';
-  const episode_trailer_location = 'Germany';
-  const episode_trailer_description = 'Blasting on the highway on my way from Manchester, UK to Bucharest, Romania';
+  const episode_trailer_location = 'Europe';
+  const episode_trailer_description = 'A short preview of the best moto riding series on YouTube';
   const episode_trailer_youTubeLink = 'https://www.youtube.com/watch?v=-dgtesVWdpc';
 
   const videoInstances = [];
