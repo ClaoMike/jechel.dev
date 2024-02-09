4082c6674f6efc09c201095de2d2cafe15053f16
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-09 19:44:35 +0200

    Rotating skills
    only once

--- jechel-dev-react/src/components/aboutMe/Skills.js
+++ jechel-dev-react/src/components/aboutMe/Skills.js
@@ -2,28 +2,33 @@ import { useSpring, animated } from 'react-spring';
 
 const Skills = () => {
 
-  function LetterAnimation({ letter }) {
+  function stringToArray(str) {
+    return str.split('');
+  }
+
+  function LetterAnimation({ letters }) {
     const props = useSpring({
-      from: { left: '-100%', position: 'absolute' },
+      from: { left: '-100%', width: `${200 * letters.length}%`, zIndex: 1 }, // Adjusted width
       to: { left: '100%' },
-      config: { duration: 3000 },
+      config: { duration: 10000 },
     });
   
     return (
-      <div style={{ overflow: 'hidden', position: 'relative', width: '100%', height: '100px' }}>
-        <animated.div style={props}>
-          {letter}
+      <div style={{ position: 'relative', width: '100%', height: '100px', overflow: 'hidden', whiteSpace: 'nowrap' }}>
+        <animated.div style={{ ...props, position: 'absolute', top: 0, zIndex: 2 }}>
+          {letters.map((letter, index) => (
+            <span key={index}>{letter}</span>
+          ))}
         </animated.div>
       </div>
     );
   }
 
+  const mySkills = "Swift | SwiftUI | UIKit | CocoaPods | fastlane | Unit/UI/Snapshot Testing | XCTest | Firebase | TestFlight | Git | CI/CD | OOP | Java | libGDX | JavaFX | JUnit | C# | Unity 2D | Python | Phaser 3 | LATEX | Procreate | Final Cut Pro | GarageBand | Frontend | Backend | Full-stack | IOS Development | Game Development | Web Development | Video Editing | English, Romanian - All professional proficiency or above";
+  const skillsArray = stringToArray(mySkills);
   return (
     <>
-    <LetterAnimation letter="A" />
-        <p>
-            Swift | SwiftUI | UIKit | CocoaPods | fastlane | Unit/UI/Snapshot Testing | XCTest | Firebase | TestFlight | Git | CI/CD | OOP | Java | libGDX | JavaFX | JUnit | C# | Unity 2D | Python | Phaser 3 | LATEX | Procreate | Final Cut Pro | GarageBand | Frontend | Backend | Full-stack | IOS Development | Game Development | Web Development | Video Editing | English, Romanian - All professional proficiency or above
-        </p>
+    <LetterAnimation letters={ skillsArray } />
     </>
   );
 };
