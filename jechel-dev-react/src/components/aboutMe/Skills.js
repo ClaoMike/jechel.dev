a17903d5d9d397d08b0820768668519088247f73
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-09 21:08:21 +0200

    Works as expected so far

--- jechel-dev-react/src/components/aboutMe/Skills.js
+++ jechel-dev-react/src/components/aboutMe/Skills.js
@@ -1,34 +1,37 @@
+import { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 
 const Skills = () => {
+  const mySkills = "Swift | SwiftUI | UIKit Swift | SwiftUI | UIKitSwift | SwiftUI | UIKitSwift | SwiftUI | UIKit";
+  const [initialPosition, setInitialPosition] = useState(-100);
 
-  function stringToArray(str) {
-    return str.split('');
-  }
+  useEffect(() => {
+    const textLength = mySkills.length;
+    setInitialPosition(-textLength * 40); // Adjust the multiplier as needed
+  }, [mySkills]);
 
-  function LetterAnimation({ letters }) {
+  function LetterAnimation() {
     const props = useSpring({
-      from: { left: '-100%', width: `${200 * letters.length}%`, zIndex: 1 }, // Adjusted width
-      to: { left: '100%' },
+      from: { transform: `translateX(${initialPosition}px)` },
+      to: { transform: 'translateX(1200%)' },
       config: { duration: 10000 },
     });
-  
+
     return (
-      <div style={{ position: 'relative', width: '100%', height: '100px', overflow: 'hidden', whiteSpace: 'nowrap' }}>
-        <animated.div style={{ ...props, position: 'absolute', top: 0, zIndex: 2 }}>
-          {letters.map((letter, index) => (
-            <span key={index}>{letter}</span>
-          ))}
+      <div style={{ overflow: 'hidden', position: 'relative', width: '100%', height: '100px', whiteSpace: 'nowrap' }}>
+        <animated.div style={props}>
+          {mySkills}
         </animated.div>
       </div>
     );
   }
 
-  const mySkills = "Swift | SwiftUI | UIKit | CocoaPods | fastlane | Unit/UI/Snapshot Testing | XCTest | Firebase | TestFlight | Git | CI/CD | OOP | Java | libGDX | JavaFX | JUnit | C# | Unity 2D | Python | Phaser 3 | LATEX | Procreate | Final Cut Pro | GarageBand | Frontend | Backend | Full-stack | IOS Development | Game Development | Web Development | Video Editing | English, Romanian - All professional proficiency or above";
-  const skillsArray = stringToArray(mySkills);
   return (
     <>
-    <LetterAnimation letters={ skillsArray } />
+      <LetterAnimation />
+      <p>
+        Swift | SwiftUI | UIKit | CocoaPods | fastlane | Unit/UI/Snapshot Testing | XCTest | Firebase | TestFlight | Git | CI/CD | OOP | Java | libGDX | JavaFX | JUnit | C# | Unity 2D | Python | Phaser 3 | LATEX | Procreate | Final Cut Pro | GarageBand | Frontend | Backend | Full-stack | IOS Development | Game Development | Web Development | Video Editing | English, Romanian - All professional proficiency or above
+      </p>
     </>
   );
 };
