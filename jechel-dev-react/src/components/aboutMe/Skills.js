16dee351381875ce43d681a371f7c485906f8db0
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-10 14:41:45 +0200

    Word entering on the left and exiting on the right

--- jechel-dev-react/src/components/aboutMe/Skills.js
+++ jechel-dev-react/src/components/aboutMe/Skills.js
@@ -3,40 +3,24 @@ import { useSpring, animated } from 'react-spring';
 
 const Skills = () => {
   const skillsIOS = ['Swift', 'SwiftUI', 'UIKit', 'CocoaPods', 'fastlane', 'Unit/UI/Snapshot Testing', 'XCTest', 'Firebase', 'TestFlight', 'Git', 'CI/CD'];
-  const skillsWEB = ['HTML', 'CSS', 'JavaScript', 'React'];
-  const gameDevSkills = ['OOP', 'Java', 'libGDX', 'C#', 'Unity 2D', 'Python', 'Phaser 3'];
-  const wordProcessingSkills = ['LATEX'];
-  const videoEditingSkills = ['Procreate','Final Cut Pro', 'GarageBand'];
-  const languages = ['English', 'Romanian'];
-  const otherSkills = ['JavaFX', 'JUnit'];
+  // const skillsWEB = ['HTML', 'CSS', 'JavaScript', 'React'];
+  // const gameDevSkills = ['OOP', 'Java', 'libGDX', 'C#', 'Unity 2D', 'Python', 'Phaser 3'];
+  // const wordProcessingSkills = ['LATEX'];
+  // const videoEditingSkills = ['Procreate','Final Cut Pro', 'GarageBand'];
+  // const languages = ['English', 'Romanian'];
+  // const otherSkills = ['JavaFX', 'JUnit'];
 
-  const [initialPosition, setInitialPosition] = useState(-100);
-
-  useEffect(() => {
-    const textLength = skillsIOS.length;
-    setInitialPosition(-textLength * 40); // Adjust the multiplier as needed
-  }, [skillsIOS]);
-
-  function LetterAnimation() {
-    const props = useSpring({
-      from: { transform: `translateX(${initialPosition}px)` },
-      to: { transform: 'translateX(1200%)' },
-      config: { duration: 10000 },
-    });
-
-    return (
-      <div style={{ overflow: 'hidden', position: 'relative', width: '100%', height: '100px', whiteSpace: 'nowrap' }}>
-        <animated.div style={props}>
-          {skillsIOS}
-        </animated.div>
-      </div>
-    );
-  }
+  const props = useSpring({
+    from: { transform: 'translateX(-100vw)' }, // Move the text by 100% of the viewport width to the left
+    to: { transform: 'translateX(100vw)' }, // Move the text by 100% of the viewport width to the right
+    loop: { reverse: true }, // Reverse animation when it reaches the end
+    config: { duration: 5000 }, // Adjust duration for the speed of movement
+  });
 
   return (
-    <>
-      <LetterAnimation />
-    </>
+    <div style={{ overflow: 'hidden', position: 'relative', width: '100%', height: '100px' }}>
+      <animated.p style={{ ...props, whiteSpace: 'nowrap', position: 'absolute' }}>{skillsIOS[0]}</animated.p>
+    </div>
   );
 };
 
