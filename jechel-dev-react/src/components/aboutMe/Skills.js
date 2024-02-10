5bba4afb8ac5123699a1ea2f3ae9a1ecc3470395
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-10 15:31:02 +0200

    Speed and translation must be dynamically calculated

--- jechel-dev-react/src/components/aboutMe/Skills.js
+++ jechel-dev-react/src/components/aboutMe/Skills.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
-import { isMobile } from 'react-device-detect';
 
 import { Stack } from '@mui/material';
 
-// 
-  // const gameDevSkills = ['OOP', 'Java', 'libGDX', 'C#', 'Unity 2D', 'Python', 'Phaser 3'];
-  // const wordProcessingSkills = ['LATEX'];
-  // const videoEditingSkills = ['Procreate','Final Cut Pro', 'GarageBand'];
-  // const languages = ['English', 'Romanian'];
-  // const otherSkills = ['JavaFX', 'JUnit'];
-
-const AnimatedSkill = ({ skill, translation }) => {
+const AnimatedSkill = ({ skill }) => {
+  const translation = 115;
+  const speed = 8000;
 
   const props = useSpring({
-    from: { transform: `translateX(-${isMobile ? translation[1] : translation[0]}vw)` },
-    to: { transform: `translateX(${isMobile ? translation[1] : translation[0]}vw)` },
+    from: { transform: `translateX(-${translation}vw)` },
+    to: { transform: `translateX(${translation}vw)` },
     loop: { reverse: true },
-    config: { duration: 8000 },
+    config: { duration: speed },
   });
 
   return (
@@ -27,11 +21,11 @@ const AnimatedSkill = ({ skill, translation }) => {
   );
 };
 
-const Skill = ({ skillsList, translation }) => {
+const Skill = ({ skillsList}) => {
   return (
     <div style={{ overflow: 'hidden', position: 'relative', width: '100%', height: '100px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       {skillsList.map((skill, index) => (
-        <AnimatedSkill key={index} skill={skill} translation={translation} />
+        <AnimatedSkill key={index} skill={skill} />
       ))}
     </div>
   );
@@ -45,11 +39,16 @@ const Skills = () => {
   ];
 
   const skill_WEB = ['HTML', 'CSS', 'JavaScript', 'React'];
+  const skill_GameDev = ['OOP', 'Java', 'libGDX', 'C#', 'Unity 2D', 'Python', 'Phaser 3'];
+  const wordProcessingSkills = ['LATEX'];
+  const videoEditingSkills = ['Procreate','Final Cut Pro', 'GarageBand'];
+  const languages = ['English', 'Romanian'];
+  const otherSkills = ['JavaFX', 'JUnit'];
 
   return (
     <Stack>
-      <Skill skillsList={skill_IOS} translation={[115, 200]}/>
-      <Skill skillsList={skill_WEB} translation={[70, 90]}/>
+      {/* TODO: translation and speed must be calculated dynamically! */}
+      <Skill skillsList={skill_IOS} /> 
     </Stack>
     
   );
