48893f4832d24d8cfc1a16d87ba29a127e5e987b
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-10 14:49:28 +0200

    The effect works

--- jechel-dev-react/src/components/aboutMe/Skills.js
+++ jechel-dev-react/src/components/aboutMe/Skills.js
@@ -1,25 +1,40 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import { useSpring, animated } from 'react-spring';
 
-const Skills = () => {
-  const skillsIOS = ['Swift', 'SwiftUI', 'UIKit', 'CocoaPods', 'fastlane', 'Unit/UI/Snapshot Testing', 'XCTest', 'Firebase', 'TestFlight', 'Git', 'CI/CD'];
-  // const skillsWEB = ['HTML', 'CSS', 'JavaScript', 'React'];
+// const skillsWEB = ['HTML', 'CSS', 'JavaScript', 'React'];
   // const gameDevSkills = ['OOP', 'Java', 'libGDX', 'C#', 'Unity 2D', 'Python', 'Phaser 3'];
   // const wordProcessingSkills = ['LATEX'];
   // const videoEditingSkills = ['Procreate','Final Cut Pro', 'GarageBand'];
   // const languages = ['English', 'Romanian'];
   // const otherSkills = ['JavaFX', 'JUnit'];
 
+const SkillItem = ({ skill }) => {
   const props = useSpring({
-    from: { transform: 'translateX(-100vw)' }, // Move the text by 100% of the viewport width to the left
-    to: { transform: 'translateX(100vw)' }, // Move the text by 100% of the viewport width to the right
-    loop: { reverse: true }, // Reverse animation when it reaches the end
-    config: { duration: 5000 }, // Adjust duration for the speed of movement
+    from: { transform: 'translateX(-150vw)' },
+    to: { transform: 'translateX(150vw)' },
+    loop: { reverse: true },
+    config: { duration: 5000 },
   });
 
   return (
-    <div style={{ overflow: 'hidden', position: 'relative', width: '100%', height: '100px' }}>
-      <animated.p style={{ ...props, whiteSpace: 'nowrap', position: 'absolute' }}>{skillsIOS[0]}</animated.p>
+    <animated.p style={{ ...props, margin: '0 10px', whiteSpace: 'nowrap' }}>
+      {skill}
+    </animated.p>
+  );
+};
+
+const Skills = () => {
+  const skillsIOS = [
+    'Swift', 'SwiftUI', 'UIKit', 'CocoaPods', 'fastlane', 
+    'Unit/UI/Snapshot Testing', 'XCTest', 'Firebase', 
+    'TestFlight', 'Git', 'CI/CD'
+  ];
+
+  return (
+    <div style={{ overflow: 'hidden', position: 'relative', width: '100%', height: '100px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {skillsIOS.map((skill, index) => (
+        <SkillItem key={index} skill={skill} />
+      ))}
     </div>
   );
 };
