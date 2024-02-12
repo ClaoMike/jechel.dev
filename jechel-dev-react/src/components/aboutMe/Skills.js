56e240f204bfba2ae772d6710a98ea848ab68850
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-12 19:13:25 +0200

    Move some of the CSS to JSX format

--- jechel-dev-react/src/components/aboutMe/Skills.js
+++ jechel-dev-react/src/components/aboutMe/Skills.js
@@ -1,17 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Stack } from '@mui/material';
+import appStyle from 'AppStyle';
 
 const Skills = () => {
   const cloudRef = useRef(null);
-  // const [leftPosition, setLeftPosition] = useState('-150vw'); // Default left position
 
   useEffect(() => {
     const handleResize = () => {
       if (cloudRef.current) {
         const width = cloudRef.current.offsetWidth;
-        const newPosition = `-${width}px`; // Calculate the new left position based on the width
-        
-        // setLeftPosition(newPosition);
+        const newPosition = `-${width}px`;
         
         const moveAnimation = `
           @keyframes move {
@@ -31,7 +29,6 @@ const Skills = () => {
     };
 
     handleResize();
-
     window.addEventListener('resize', handleResize);
 
     return () => {
@@ -41,8 +38,8 @@ const Skills = () => {
 
   return (
     <Stack>
-      <div className='cloud-container'>
-        <div ref={cloudRef} className='cloud'> Swift SwiftUI UIKit CocoaPods fastlane Unit/UI/Snapshot Testing XCTest Firebase TestFlight Git CI/CD </div>
+      <div style={appStyle.skillContainer}>
+        <div ref={cloudRef} style={appStyle.skill}> Swift SwiftUI UIKit CocoaPods fastlane Unit/UI/Snapshot Testing XCTest Firebase TestFlight Git CI/CD </div>
       </div>
     </Stack>
   );
