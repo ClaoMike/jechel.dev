5b19102e8710ff379bf39ad2bcc947ce5de5063b
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-12 19:06:24 +0200

    Add space between the header and the skills sections

--- jechel-dev-react/src/components/aboutMe/AboutMeHeader.js
+++ jechel-dev-react/src/components/aboutMe/AboutMeHeader.js
@@ -6,8 +6,8 @@ import appStyle from "AppStyle";
 const AboutMeHeader = () => {
   return (
     <>
-    <Stack direction="column" justifyContent="center" alignItems="center" spacing={2}>
-     <img src='resources/me.JPEG' alt='Photo of me' style={appStyle.mePhoto}/>
+    <Stack direction="column" justifyContent="center" alignItems="center" spacing={2} style={appStyle.headerConfig}>
+      <img src='resources/me.JPEG' alt='Photo of me' style={appStyle.mePhoto}/>
 
       <Stack direction="row" justifyContent="center" alignItems="center" spacing={0}>
         <ExternalLink link='resources/Claudiu_Mihai_Jechel_Resume.pdf' text='Resume' icon={<ResumeIcon />}/> &nbsp; 
