5f40abd5eb735ba02690afec46097ce9ce5e1224
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-15 20:47:20 +0200

    Removed Warning

--- jechel-dev-react/src/components/aboutMe/AboutMeHeader.js
+++ jechel-dev-react/src/components/aboutMe/AboutMeHeader.js
@@ -7,7 +7,7 @@ const AboutMeHeader = () => {
   return (
     <>
     <Stack direction="column" justifyContent="center" alignItems="center" spacing={2} style={appStyle.headerConfig}>
-      <img src='resources/me.JPEG' alt='Photo of me' style={appStyle.mePhoto}/>
+      <img src='resources/me.JPEG' alt='Me' style={appStyle.mePhoto}/>
 
       <Stack direction="row" justifyContent="center" alignItems="center" spacing={0}>
         <ExternalLink link='resources/Claudiu_Mihai_Jechel_Resume.pdf' text='Resume' icon={<ResumeIcon />}/> &nbsp; 
