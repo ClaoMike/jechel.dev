95c769189921b0f759008ea82d873c05dbf5e093
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-15 22:25:58 +0200

    Update AcademicPapers.js

--- jechel-dev-react/src/components/aboutMe/AcademicPapers.js
+++ jechel-dev-react/src/components/aboutMe/AcademicPapers.js
@@ -1,15 +1,16 @@
+import appStyle from "AppStyle";
 import { ExternalLink } from "Components";
 
 const AcademicPapers = () => {
   return (
-    <>
-        <h3>Academic papers</h3>
+    <div style={appStyle.skillsSection}>
+        <h2>Academic papers</h2>
         <ul>
             <li>
                 <ExternalLink link='resources/Thesis.pdf' text='How to build a video game using Procedural Content Generation?' />
             </li>
         </ul>
-    </>
+    </div>
   );
 };
 
