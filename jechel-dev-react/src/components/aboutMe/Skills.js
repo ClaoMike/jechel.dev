6b8932712eae253c5dd0cffd8dd3f5fb5b4cf94e
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-15 11:58:11 +0200

    Refactor skills

--- jechel-dev-react/src/components/aboutMe/Skills.js
+++ jechel-dev-react/src/components/aboutMe/Skills.js
@@ -3,44 +3,16 @@ import { Stack } from '@mui/material';
 import appStyle from 'AppStyle';
 
 const Skills = () => {
-  const cloudRef = useRef(null);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (cloudRef.current) {
-        const width = cloudRef.current.offsetWidth;
-        
-        const moveAnimation = `
-          @keyframes move {
-            from {
-              left: -${width}px;
-            }
-            to {
-              left: 100vw;
-            }
-          }
-        `;
-
-        const styleElement = document.createElement('style');
-        styleElement.textContent = moveAnimation;
-        document.head.appendChild(styleElement);
-      }
-    };
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  const skill_IOS = [
+    'Swift', 'SwiftUI', 'UIKit', 'CocoaPods', 'fastlane', 'Unit/UI/Snapshot Testing', 'XCTest', 'Firebase', 'TestFlight', 'Git', 'CI/CD'];
+  const skill_WEB = ['HTML', 'CSS', 'JavaScript', 'React'];
+  const gameDevSkills = ['OOP', 'Java', 'libGDX', 'C#', 'Unity 2D', 'Python', 'Phaser 3'];
+  const wordProcessingSkills = ['LATEX'];
+  const videoEditingSkills = ['Procreate','Final Cut Pro', 'GarageBand'];
 
   return (
-    <Stack>
-      <div style={appStyle.skillContainer}>
-        <div ref={cloudRef} style={appStyle.skill}> Swift SwiftUI UIKit CocoaPods fastlane Unit/UI/Snapshot Testing XCTest Firebase TestFlight Git CI/CD </div>
-      </div>
-    </Stack>
+    <>
+    </>
   );
 };
 
