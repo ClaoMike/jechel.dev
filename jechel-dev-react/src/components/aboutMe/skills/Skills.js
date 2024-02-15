1550025850e45f8ee9ecdc9911d2453e3071da77
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-15 23:17:55 +0200

    move colours to variables

--- jechel-dev-react/src/components/aboutMe/skills/Skills.js
+++ jechel-dev-react/src/components/aboutMe/skills/Skills.js
@@ -1,46 +1,60 @@
+const colours = {
+  colour1: '#8E7AB5',
+  colour2: '#B784B7',
+  colour3: '#E493B3',
+  colour4: '#EEA5A6',
+  colour5: '#EC8F5E',
+  colour6: '#11235A',
+  colour7: '#11235A',
+  colour8: '#9FBB73',
+  colour9: '#004225',
+  colour10: '#8CB9BD',
+  colour11: '#FFB000',
+};
+
 const skills = {
     data_IOS: [
-        { title: 'Swift', value: 9, color: '#8E7AB5' },
-        { title: 'SwiftUI', value: 9, color: '#B784B7' },
-        { title: 'UIKit', value: 9, color: '#E493B3' },
-        { title: 'CocoaPods', value: 9, color: '#EEA5A6' },
+        { title: 'Swift',                     value: 9,   color: colours.colour1 },
+        { title: 'SwiftUI',                   value: 9,   color: colours.colour2 },
+        { title: 'UIKit',                     value: 9,   color: colours.colour3 },
+        { title: 'CocoaPods',                 value: 9,   color: colours.colour4 },
     
-        { title: 'fastlane', value: 9, color: '#EC8F5E' },
-        { title: 'Unit/UI/Snapshot Testing', value: 9, color: '#F3B664' },
-        { title: 'XCTest', value: 9, color: '#11235A' }, //
-        { title: 'Firebase', value: 9, color: '#9FBB73' },
+        { title: 'fastlane',                  value: 9,   color: colours.colour5 },
+        { title: 'Unit/UI/Snapshot Testing',  value: 9,   color: colours.colour6 },
+        { title: 'XCTest',                    value: 9,   color: colours.colour7 },
+        { title: 'Firebase',                  value: 9,   color: colours.colour8 },
     
-        { title: 'TestFlight', value: 9, color: '#004225' },
-        { title: 'git', value: 9, color: '#8CB9BD' }, //
-        { title: 'CI/CD', value: 10, color: '#FFB000' },
+        { title: 'TestFlight',                value: 9,   color: colours.colour9 },
+        { title: 'git',                       value: 9,   color: colours.colour10 },
+        { title: 'CI/CD',                     value: 10,  color: colours.colour11 },
       ],
     
       data_WEB: [
-        { title: 'HTML', value: 25, color: '#8E7AB5' },
-        { title: 'CSS', value: 25, color: '#B784B7' },
-        { title: 'JavaScript', value: 25, color: '#E493B3' },
-        { title: 'React', value: 25, color: '#EEA5A6' },
+        { title: 'HTML',                      value: 25,  color: colours.colour1 },
+        { title: 'CSS',                       value: 25,  color: colours.colour2 },
+        { title: 'JavaScript',                value: 25,  color: colours.colour3 },
+        { title: 'React',                     value: 25,  color: colours.colour4 },
       ],
     
       data_GAMEDEV: [
-        { title: 'OOP', value: 14, color: '#8E7AB5' },
-        { title: 'Java', value: 14, color: '#B784B7' },
-        { title: 'libGDX', value: 14, color: '#E493B3' },
-        { title: 'C#', value: 14, color: '#EEA5A6' },
+        { title: 'OOP',                       value: 14,  color: colours.colour1 },
+        { title: 'Java',                      value: 14,  color: colours.colour2 },
+        { title: 'libGDX',                    value: 14,  color: colours.colour3 },
+        { title: 'C#',                        value: 14,  color: colours.colour4 },
     
-        { title: 'Unity 2D', value: 14, color: '#EC8F5E' },
-        { title: 'Python', value: 14, color: '#F3B664' },
-        { title: 'Phaser 3', value: 16, color: '#11235A' },//
+        { title: 'Unity 2D',                  value: 14,  color: colours.colour5 },
+        { title: 'Python',                    value: 14,  color: colours.colour6 },
+        { title: 'Phaser 3',                  value: 16,  color: colours.colour7 },
       ],
     
       data_WORDPROCESSING: [
-        { title: 'LATEX', value: 100, color: '#8E7AB5' },
+        { title: 'LATEX',                     value: 100, color: colours.colour1 },
       ],
     
       data_VIDEOEDITING: [
-        { title: 'Procreate', value: 33, color: '#8E7AB5' },
-        { title: 'Final Cut Pro', value: 33, color: '#B784B7' },
-        { title: 'GarageBand', value: 34, color: '#E493B3' }
+        { title: 'Procreate',                 value: 33,  color: colours.colour1 },
+        { title: 'Final Cut Pro',             value: 33,  color: colours.colour2 },
+        { title: 'GarageBand',                value: 34,  color: colours.colour3 }
       ],
 
 };
