06f305d77924cef1a03447c9a84759eb669aed7f
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-16 13:05:03 +0200

    Add tooltip

--- jechel-dev-react/src/components/main/VersionSection.js
+++ jechel-dev-react/src/components/main/VersionSection.js
@@ -1,10 +1,13 @@
 import appStyle from 'AppStyle'
 import React from 'react'
+import Tooltip from '@mui/material/Tooltip';
 
 const VersionSection = () => {
-  return (
-    <div style={appStyle.horizontalCenterStyle}>
-        <p> Version 1.0.0 </p>
+    return (
+        <div style={appStyle.horizontalCenterStyle}>
+            <Tooltip title="Delete">
+                <p> Version 1.0.0 </p>
+            </Tooltip>
     </div>
   )
 }
