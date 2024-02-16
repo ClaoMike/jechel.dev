86ecbdc5edd5d092c70b8196a9d8ba0c5b7f5f9c
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-02-16 13:40:55 +0200

    Add icons

--- jechel-dev-react/src/components/main/VersionSection.js
+++ jechel-dev-react/src/components/main/VersionSection.js
@@ -1,11 +1,29 @@
 import appStyle from 'AppStyle'
 import React from 'react'
 import Tooltip from '@mui/material/Tooltip';
+import { Stack } from '@mui/material';
+
+import HtmlIcon from '@mui/icons-material/Html';
+import CssIcon from '@mui/icons-material/Css';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import { FaReact } from "react-icons/fa";
 
 const VersionSection = () => {
     return (
         <div style={appStyle.horizontalCenterStyle}>
-            <Tooltip title="Delete">
+            <Tooltip placement="top" title={
+                <Stack
+                direction="row"
+                justifyContent="space-evenly"
+                alignItems="center"
+                spacing={1}
+                >
+                    <FaReact/>
+                    <JavascriptIcon />
+                    <HtmlIcon />
+                    <CssIcon/>
+                </Stack>
+            }>
                 <p> Version 1.0.0 </p>
             </Tooltip>
     </div>
