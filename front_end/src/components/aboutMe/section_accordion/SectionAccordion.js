4bd5cd9b90474ffbd9fc90ce2321610e6007f8d7
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-10 13:58:48 +0300

    Move css to style page

--- front_end/src/components/aboutMe/section_accordion/SectionAccordion.js
+++ front_end/src/components/aboutMe/section_accordion/SectionAccordion.js
@@ -5,13 +5,11 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import style from './SectionAccordionStyle';
 
-import { isMobile } from 'react-device-detect';
-
 const SectionAccordion = ({header, content}) => {
   return (
-    <Accordion defaultExpanded style={style}>
+    <Accordion defaultExpanded style={style.accordion}>
 
-      <div style={{ backgroundColor: 'black', paddingLeft: '20%',paddingRight: '20%', marginLeft: '-20%', marginRight: '-20%', position: 'sticky', top: isMobile ? '3%' : '7%', zIndex: 999}}>
+      <div style={style.accordion_summary}>
         <AccordionSummary 
           className='accordion_summary' 
           expandIcon={<ExpandMoreIcon className='episode-text-font episode-text-colour' />}
--- front_end/src/components/aboutMe/section_accordion/SectionAccordionStyle.js
+++ front_end/src/components/aboutMe/section_accordion/SectionAccordionStyle.js
@@ -1,10 +1,23 @@
 import { isMobile } from 'react-device-detect';
 
 const style = {
-        marginLeft: isMobile ? '5vw' :'10vw',
-        marginRight: isMobile ? '5vw' :'10vw',
-        backgroundColor: 'transparent',
-        padding: '0px'
+        accordion: {
+                marginLeft: isMobile ? '5vw' :'10vw',
+                marginRight: isMobile ? '5vw' :'10vw',
+                backgroundColor: 'transparent',
+                padding: '0px'
+        },
+        accordion_summary: {
+                backgroundColor: 'black', 
+                paddingLeft: '20%',
+                paddingRight: '20%',
+                marginLeft: '-20%', 
+                marginRight: '-20%', 
+                position: 'sticky', 
+                top: isMobile ? '3%' : '7%', 
+                zIndex: 999
+        }
+       
         
 }
 
