f0afcfc22b2f3319b3acb352bcf08f3c09a2fa3a
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-10 13:49:41 +0300

    Fix positioning for the accordion summary

--- front_end/src/components/aboutMe/section_accordion/SectionAccordion.js
+++ front_end/src/components/aboutMe/section_accordion/SectionAccordion.js
@@ -5,18 +5,22 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import style from './SectionAccordionStyle';
 
+import { isMobile } from 'react-device-detect';
+
 const SectionAccordion = ({header, content}) => {
   return (
     <Accordion defaultExpanded style={style}>
 
-      <AccordionSummary 
-        className='accordion_summary' 
-        expandIcon={<ExpandMoreIcon className='episode-text-font episode-text-colour' />}
-        style={{ position: 'sticky', top: 30, zIndex: 999 }}
-      >
+      <div style={{ backgroundColor: 'black', paddingLeft: '20%',paddingRight: '20%', marginLeft: '-20%', marginRight: '-20%', position: 'sticky', top: isMobile ? '3%' : '7%', zIndex: 999}}>
+        <AccordionSummary 
+          className='accordion_summary' 
+          expandIcon={<ExpandMoreIcon className='episode-text-font episode-text-colour' />}
+        >
         {header}
       </AccordionSummary>
         
+      </div>
+      
       <AccordionDetails>
         {content}
       </AccordionDetails>
