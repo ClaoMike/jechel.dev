88174e4e9f92ae85c19a457a578ae709736515f1
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-25 13:42:01 +0300

    Update animation size

--- front_end/src/app/components/website_loader/WebsiteLoaderStyle.jsx
+++ front_end/src/app/components/website_loader/WebsiteLoaderStyle.jsx
@@ -8,16 +8,16 @@ const style = {
         viewBox: "0 0 100 100",
 
         greater_sign: {
-            x: "10",
+            x: "0",
             y: "50",
-            fontSize: "40",
+            fontSize: "100",
             fill: colors.website_loader.greater_sign,
         },
 
         underscore_sign: {
-            x: "30",
+            x: "40",
             y: "50",
-            fontSize: "40",
+            fontSize: "100",
             fill: colors.website_loader.underscore_sign
         }
     },
