9618c481b98acdd282819f000e0097279eb54ed2
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-25 13:53:13 +0300

    Prepared routes

--- front_end/src/app/components/Contents.jsx
+++ front_end/src/app/components/Contents.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Center, VStack } from '@chakra-ui/react';
 import Header from './header/Header';
+import UserContent from './UserContent';
+import LoginPage from './LoginPage';
 
 const Contents = () => {
   return (
@@ -8,6 +11,13 @@ const Contents = () => {
       <VStack mt="5vh" mb="5vh">
         
         <Header/>
+        
+        <Router>
+          <Routes>
+            <Route path='/*' element={<UserContent />} />
+            <Route path='/admin' element={<LoginPage />} />
+          </Routes>
+        </Router>
         
       </VStack>    
     </Center>
