ed423deef7a933a32dadbfe6840c0ab10611e681
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-25 14:03:07 +0300

    Login page layout

--- front_end/src/app/components/LoginPage.jsx
+++ front_end/src/app/components/LoginPage.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    Button,
+    Stack,
+} from '@chakra-ui/react';
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Stack spacing={4}>
+
+                <FormControl id="username">
+                    <FormLabel>Username</FormLabel>
+                    <Input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Enter your username"
+                    />
+                </FormControl>
+
+                <FormControl id="password">
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter your password"
+                    />
+                </FormControl>
+
+                <Button type="submit" colorScheme="blue">
+                    Submit
+                </Button>
+
+            </Stack>
+        </form>
+    )
 }
 
 export default LoginPage
