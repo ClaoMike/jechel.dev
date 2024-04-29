762c5c5e2bc4a886eb9ea1b363301180a3e15d72
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-29 20:22:12 +0300

    Prepared data display

--- front_end/src/app/components/content/user_content/content_pages/MotoPage.jsx
+++ front_end/src/app/components/content/user_content/content_pages/MotoPage.jsx
@@ -6,6 +6,18 @@ import { useSelector, useDispatch } from 'react-redux';
 import LoadingSpinner from '@/app/components/LoadingSpinner';
 import ErrorDisplay from '@/app/components/ErrorDisplay';
 
+import {
+    Accordion,
+    AccordionItem,
+    AccordionButton,
+    AccordionPanel,
+    AccordionIcon,
+    Box
+  } from '@chakra-ui/react'
+
+  import { Image, HStack, VStack } from '@chakra-ui/react'
+  import { Text } from '@chakra-ui/react'
+
 const MotoPage = () => {
     const isLoading = useSelector((state) => state.moto.loading);
     const videos = useSelector((state) => state.moto.videos);
@@ -20,16 +32,12 @@ const MotoPage = () => {
                 console.log(data)
             } catch (error) {
                 dispatch(errorAction(error))
-                // console.log(error)
             }
         };
 
         fetchData();
 
-        // Cleanup function (optional)
-        return () => {
-            // Perform cleanup if necessary
-        };
+        return () => {};
     }, []);
 
     if (isLoading) {
@@ -44,16 +52,45 @@ const MotoPage = () => {
     }
 
     return (
-        <div>
-            {/* Display Moto videos */}
-            <ul>
-                {videos.map(video => (
-                    <li key={video.position}>{video.title}</li>
-                ))}
-            </ul>
-        </div>
+        <>
+            {videos.map(video => (
+                <HStack key={video.position}>
+                    <Image  src={video.thumbnail} alt={video.description} />
+                    <Text >{video.title}</Text>
+                    <VStack>
+                        <Text >{video.description}</Text>
+                        <Text >{video.location}</Text>
+                        <Text >{video.url}</Text>
+                    </VStack>
+                    
+                </HStack>
+                
+            ))}
+            </>
     );
 }
 
 
 export default MotoPage;
+
+{/* <Accordion>
+
+  <AccordionItem>
+      <AccordionButton>
+        <HStack>
+            <Image  src={video.thumbnail} alt={video.description} />
+            <Text >{video.title}</Text>
+        </HStack>
+        <AccordionIcon />
+    </AccordionButton>
+
+    <AccordionPanel>
+        <VStack>
+            <Text >{video.description}</Text>
+            <Text >{video.location}</Text>
+            <Text >{video.url}</Text>
+        </VStack>
+    </AccordionPanel>
+  </AccordionItem>
+
+</Accordion> */}
