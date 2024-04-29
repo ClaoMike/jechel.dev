d6d23f51ea9eec01d9c01970601e5eaf0bf3ec88
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2024-04-29 12:52:21 +0300

    Selecting highest thumbnail res available

--- back_end/app/controllers/moto_controller.rb
+++ back_end/app/controllers/moto_controller.rb
@@ -10,8 +10,6 @@ class MotoController < ApplicationController
         playlist_id = ENV['PLAYLIST_ID']
         videos_response = fetch_all_playlist_videos(youtube, playlist_id)
 
-        # render json: { videos: videos_response }
-
         # Extract video information from the response
         if videos_response.items.any?
             videos = videos_response.items.map do |item|
@@ -21,7 +19,7 @@ class MotoController < ApplicationController
                     position: item.snippet.position,
                     video_id: item.snippet.resource_id.video_id,
                     url: "https://www.youtube.com/watch?v=#{item.snippet.resource_id.video_id}",
-                    thumbnail: item.snippet.thumbnails.high.url,
+                    thumbnail: get_highest_quality_thumbnail(item.snippet.thumbnails),
                     description: item.snippet.description,
                     location: get_video_location(youtube, item.snippet.resource_id.video_id).items.first.recording_details.location_description
                 }
@@ -51,8 +49,16 @@ class MotoController < ApplicationController
         end
     end
 
+    # Method to fetch the location of a video
     def get_video_location(youtube, video_id)
         youtube.list_videos('recordingDetails', id: video_id)
     end
 
+    # Method to get the highest quality thumbnail URL
+    def get_highest_quality_thumbnail(thumbnails)
+        %w(maxres high medium standard default).each do |quality|
+            return thumbnails.public_send(quality).url if thumbnails.public_send(quality)&.url
+        end
+        nil
+    end
 end
